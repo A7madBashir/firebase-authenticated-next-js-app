3e6c7ec4d6c463048d9de3f1bb18ec16752159cd
A7madBashir <ahmad.bashir.ash@gmail.com>
2024-09-28 18:59:05 +0300

    update(hooks): local storage check if window is defined for server side

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,14 +1,21 @@
+"use client";
 import { useState, useEffect } from "react";
 import { LocalStorage } from "../models/DTO/localStorage";
+import { isEmpty } from "../utils/isEmpty";
 
 function getStorageValue({ key, value }: LocalStorage) {
+  if (typeof window == typeof undefined) return "";
+  
   // getting stored value
-  const saved = localStorage.getItem(key) ?? "";
-  const initial = JSON.parse(saved);
+  const saved = localStorage.getItem(key);
+  const initial = isEmpty(saved) ? "" : JSON.parse(saved!);
   return initial || value;
 }
 
-export const useLocalStorage = ({ key, value: defaultValue }: LocalStorage) => {
+export default function useLocalStorage({
+  key,
+  value: defaultValue,
+}: LocalStorage) {
   const [value, setValue] = useState(() => {
     return getStorageValue({ key, value: defaultValue });
   });
@@ -18,4 +25,4 @@ export const useLocalStorage = ({ key, value: defaultValue }: LocalStorage) => {
   }, [key, value]);
 
   return [value, setValue];
-};
+}
